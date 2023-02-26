7cf8445f957583be14e26047d64ef2916a50daad
Merve Şahin <merve_sahin_2002@hotmail.com>
2023-02-26 16:14:58 +0300

    first commit

--- src/ChallangeMain.java
+++ src/ChallangeMain.java
@@ -34,6 +34,7 @@ public class ChallangeMain {
 			System.out.println("\n\nEnter a number to see its reverse (Enter 0 to stop):");
 			numStr=input.nextLine();
 		}
+		
 		System.out.println("\nProgram Stopped!");
 	}
 }
